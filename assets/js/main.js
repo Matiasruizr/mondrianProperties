f5f2f9dd0d9acbf78477b86730a249af20341d45
Matias <matiasruizr@hotmail.es>
2018-11-20 00:46:46 -0400

    Se quito scss para entrada de Rain a maquetar

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,5 +58,28 @@ $( document ).ready(function() {
        }
      }
    });
-   
+   // Testimonial section
+   $("#testimonial-slider").owlCarousel({
+
+    items:1,
+
+    itemsDesktop:[1000,1],
+
+    itemsDesktopSmall:[979,1],
+
+    itemsTablet:[768,1],
+
+    pagination:true,
+
+    navigation:false,
+
+    slideSpeed:1000,
+
+    singleItem:false,
+
+    navigationText:["",""],
+
+    autoPlay:true
+
+  });
 });
